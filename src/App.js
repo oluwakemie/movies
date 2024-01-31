238f39fe9f114e8175a56197b831db3fa4453c9e
Oluwakemi <oluwakemie05@gmail.com>
2024-01-31 15:32:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import MovieBox from "./MovieBox";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+const API_URL =
+  "https://api.themoviedb.org/3/movie/popular?api_key=4d9b89b09ef2f50c6be374c6da1d78f0";
+const API_SEARCH =
+  "'https://api.themoviedb.org/3/search/movie?api_key=4d9b89b09ef2f50c6be374c6da1d78f0&query";
+// "https://api.themoviedb.org/3/movie/11?api_key=4d9b89b09ef2f50c6be374c6da1d78f0";
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }, []);
+
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    console.log("Searching");
+    try {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=4d9b89b09ef2f50c6be374c6da1d78f0&query=${query}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setMovies(data.results);
+    } catch (e) {
+      console.log(e.target.value);
+    }
+  };
+  const changeHandler = (e) => {
+    setQuery(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container fuild>
+          <Navbar.Brand href="/home">MovieDb App</Navbar.Brand>
+          <Navbar.Brand href="/home">Trending</Navbar.Brand>
+          <Navbar.Toggle aria_controls="navbarScroll"> </Navbar.Toggle>
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-3"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            ></Nav>
+            <Form className="d-flex" onSubmit={searchMovie}>
+              <FormControl
+                type="search"
+                placeholder="Movie Search"
+                className="me-2"
+                aria-label="search"
+                name="query"
+                value={query}
+                onChange={changeHandler}
+              ></FormControl>
+              <Button variant="secondary" type="submit">
+                Search
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>{" "}
+      <div>
+        {movies.length > 0 ?(<div className="container">
+          <div className="grid">
+            {movies.map((movieReq) => (
+              <MovieBox key={movieReq.id} {...movieReq} />
+            ))}
+          </div>
+        </div>):(
+          <h2>Sorry !! No Movies Found</h2>
+        )}
+        
+      </div>
+    </>
   );
 }
 
---
+++ src/MovieBox.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Modal from 'react-bootstrap/Modal';
+import Button from'react-bootstrap/Button';
+const API_IMG="https://image.tmdb.org/t/p/w500/";
+const MovieBox = ({ title, poster_path, vote_average, release_date, overview
+}) => {
+ 
+  const [show, setShow]=useState(false)
+  
+  const handleShow=()=>setShow(true);
+  const handleClose=()=>setShow(false);
+  return (
+    <div className="card text-center bg-secondary mb-3"> 
+    <div className="card-body">
+      <img className="card-img-top" src={API_IMG+poster_path} alt="" />
+   <div className="card-body">
+      <button type="button" className="btn btn-dark" onClick={handleShow}>View More</button>
+   <Modal show={show} onHide={handleClose}> 
+     <Modal.Header closeButton>
+     <Modal.Title></Modal.Title>
+     </Modal.Header>
+     <Modal.Body>
+      <img className="card-img-top" style={{width: "14rem"}} src={API_IMG+poster_path}  alt="" />
+      <h3>{title}</h3>
+      <h4>ImDb: {vote_average}</h4>
+      <h5>Release Date: {release_date}</h5>
+      <br />
+      <h6>Overview</h6>
+      <p>{overview}</p>
+     </Modal.Body>
+     <Modal.Footer>
+      <Button variant="secondary" onClick={handleClose}>close</Button>
+     </Modal.Footer>
+   </Modal>
+   </div>
+    </div>
+  
+    </div>
+  );
+};
+
+export default MovieBox;
